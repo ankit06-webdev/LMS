e74d97b8b50dbad36b8b7dd12576589f2a54575f
ankit06-webdev <webdeveloperr06@gmail.com>
2025-05-01 16:04:17 +0530

    mongodb connected

--- conn.js
+++ conn.js
@@ -6,7 +6,10 @@ const connectDB = async () => {
   try {
     // await mongoose.connect('mongodb://localhost:27017/LMS');
     // await mongoose.connect('mongodb+srv://webdeveloperr06:webDev2005@cluster0.rndtz9m.mongodb.net/');
-    await mongoose.connect(mongoURL);
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected.");
   } catch (err) {
     console.error("MongoDB connection error:", err);
